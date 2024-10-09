952ab48b47a89075b3227adedb36e7c4253becef
Saim ALi <saim.ali.siddiqui2004@gmail.com>
2024-10-09 19:23:36 +0500

    v1

--- js/get-all-jobs.js
+++ js/get-all-jobs.js
@@ -185,3 +185,49 @@ async function subscribeToEmail() {
         }).showToast();
     }
 }
+
+async function subscribeToEmailBox() {
+    console.log("A")
+    const email = document.getElementById('newsletter-email').value;
+
+    if (!email) {
+        Toastify({
+            text: "Please enter a valid email address",
+            backgroundColor: "linear-gradient(to right, #ff5f6d, #ffc371)",
+            duration: 3000
+        }).showToast();
+        return;
+    }
+
+    try {
+        const response = await fetch('https://minijob-backend.vercel.app/api/v1/email-subscribe/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+        });
+
+        const data = await response.json();
+
+        if (response.status === 201) {
+            Toastify({
+                text: "Subscribed to email successfully!",
+                backgroundColor: "linear-gradient(to right, #ff5f6d, #ffc371)",
+                duration: 3000
+            }).showToast();
+        } else {
+            Toastify({
+                text: `Error: ${data.message}`,
+                backgroundColor: "linear-gradient(to right, #ff5f6d, #ffc371)",
+                duration: 3000
+            }).showToast();
+        }
+    } catch (error) {
+        Toastify({
+            text: "Something went wrong. Please try again later.",
+            backgroundColor: "linear-gradient(to right, #ff5f6d, #ffc371)",
+            duration: 3000
+        }).showToast();
+    }
+}
